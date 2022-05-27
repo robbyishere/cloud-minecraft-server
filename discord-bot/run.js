1b1b9b94287a7a213ee2e406708ddd653c9d69c6
robbyishere <jkoh1295@gmail.com>
2022-05-26 20:43:32 -0700

    Parse JSON to find VM status, Different reply message when server has already started or for errors, and errors are logged to console

--- discord-bot/run.js
+++ discord-bot/run.js
@@ -2,19 +2,35 @@ const dotenv = require('dotenv/config');
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const {exec} = require('child_process');
+var serverStatus=0;
 
 function statusCheck(){
 //Changes Discord Bot's online status to reflect VM's status
-	exec("az vm list -d -g Minecraft-Server || grep Minecraft-Server  VM RUNNING",(err,stdout) => {
-		  if(!err){
-			if(stdout){
+	exec("az vm list -d -g Minecraft-Server",(err,stdout) => {
+		if(!err){
+			var json=JSON.parse(stdout);
+			var vmStatus=json[0].powerState
+			if(vmStatus=='VM running'||vmStatus=='VM starting'){
 				client.user.setStatus('online');
+				serverStatus=1;
+			}
+			else if(vmStatus=='VM stopped'){
+				client.user.setStatus('dnd');
+				serverStatus=0;
 			}
 			else{
-				client.user.setStatus('dnd');
+				client.user.setStatus('invisible');
+				console.log("Other Power State: "+vmStatus);
+				serverStatus=0;
 			}
 		  }
-	  });
+		  else{
+			  console.log("Error\n"+err);
+			  console.log("Output\n"+stdout);
+
+			  client.user.setStatus('invisible');
+		  }
+	});
 	setTimeout(statusCheck,60000);
 }
 	
@@ -29,9 +45,28 @@ client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	if (interaction.commandName === 'start') {
+		var reply="Reply not set";
+		const successStart="Starting the Minecraft Server :sunglasses: \n*Please wait as this can take a few minutes*";
+		const alreadyStart="What are you doing???\nThe Server already started :stuck_out_tongue:";
+		const tryAgain="Please wait a minute and try again";
+		
 		console.log(time.getFullYear()+"-"+(time.getMonth()+1)+"-"+time.getDate()+" "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()+" "+interaction.user.username+" "+"/start");
-		exec('az vm start -g Minecraft-Server -n Minecraft-VM');
-		await interaction.reply({content: 'Starting the Minecraft Server :sunglasses:' +'\n'+'*Please wait as this can take a few minutes*', ephemeral: true});
+		if(serverStatus==0){
+			exec('az vm start -g Minecraft-Server -n Minecraft-VM',(err,stdout) => {
+				if(err) {
+					console.log("VM Start Failed");
+					console.log("Error\n"+err);
+					console.log("Output\n"+stdout);
+					interaction.followUp({content: "Server failed to start :disappointed:", ephemeral: true});
+				}
+			});
+			reply=successStart;
+		}
+		else{
+			reply=alreadyStart;
+		}
+		await interaction.reply({content: reply, ephemeral: true});
+	}
 	}
 	if (interaction.commandName === 'address') {
 		console.log(time.getFullYear()+"-"+(time.getMonth()+1)+"-"+time.getDate()+" "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()+" "+interaction.user.username+" "+"/address");
